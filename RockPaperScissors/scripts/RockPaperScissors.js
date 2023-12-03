db047ebcb4787cdf53d366147b7d7247fa7af7eb
Supeep <supersheep233@gmail.com>
2023-12-03 11:47:12 +0800

    finish RockPaperScissors

--- RockPaperScissors/scripts/RockPaperScissors.js
+++ RockPaperScissors/scripts/RockPaperScissors.js
@@ -1,10 +1,30 @@
-let score={
+let score=JSON.parse(localStorage.getItem('score'))||{
     wins:0,
     losses:0,
     ties:0
 }
 
+
 updateScoreElement();
+
+let isAutoPlaying=false;
+let intervalId;
+
+function autoPlay(){
+    if(!isAutoPlaying){
+        intervalId=setInterval(()=>{
+            const playerMove =ComputerMove();
+            playGame(playerMove);
+        },500);
+        isAutoPlaying=true;
+        document.querySelector('.auto-play-button').innerHTML=`Stop Playing`;
+
+    }else{
+        clearInterval(intervalId);
+        document.querySelector('.auto-play-button').innerHTML=`Auto Play`;
+        isAutoPlaying=false;
+    }
+}
 
 document.querySelector('.js-rock-button')
     .addEventListener('click',()=>{ 
@@ -20,6 +40,20 @@ document.querySelector('.js-scissors-button')
     .addEventListener('click',()=>{
         playGame('scissors');
     });
+
+document.querySelector('.js-reset-score-button')
+    .addEventListener('click',()=>{
+        score.wins=0;
+        score.losses=0;
+        score.ties=0;
+        localStorage.removeItem('score');
+        updateScoreElement();
+})
+
+document.querySelector('.auto-play-button')
+    .addEventListener('click',()=>{
+        autoPlay();
+    })
 
 function playGame(playerMove){
     const computerMove=ComputerMove();
@@ -60,7 +94,16 @@ function playGame(playerMove){
         score.ties += 1;
     }
 
+    localStorage.setItem('score',JSON.stringify(score));
+
     updateScoreElement();
+
+    document.querySelector('.js-result').innerHTML=result;
+
+    document.querySelector('.js-moves').innerHTML=`You
+<img src="images/${playerMove}-emoji.png" class="move-icon">
+<img src="images/${computerMove}-emoji.png" class="move-icon">
+Computer`;
 }
 
 function ComputerMove(){
